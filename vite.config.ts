c3aa5ce4b171bd9aeb1154953cd41b54edb61be6
brusiqueira9 <siqueirabruno455@gmail.com>
2025-05-06 15:58:52 -0300

    Nova Função PWA

--- vite.config.ts
+++ vite.config.ts
@@ -21,4 +21,18 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+      },
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+        },
+      },
+    },
+  },
 }));
